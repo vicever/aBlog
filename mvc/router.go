9894373ce62014e1c1b33062345478c465927e60
fuxiaohei <fuxiaohei@vip.qq.com>
2015-03-01 23:12:17 +0800

    update tag struct

--- mvc/router.go
+++ mvc/router.go
@@ -13,6 +13,8 @@ func Init() {
 	apiGroup.Any("/user/info", new(api.UserInfoController))
 	apiGroup.Post("/user/password", new(api.UserPasswordController))
 	apiGroup.Any("/category", new(api.CategoryController))
-	apiGroup.Any("/categories", new(api.CategoriesController))
+	apiGroup.Get("/categories", new(api.CategoriesController))
+	apiGroup.Any("/tag", new(api.TagController))
+	apiGroup.Get("/tags", new(api.TagsController))
 	core.Server.Group("/api", apiGroup)
 }
