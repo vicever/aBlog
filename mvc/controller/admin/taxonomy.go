5162f8a335f8c692127b62b45fb05c1c6cc7c7fe
fuxiaohei <fuxiaohei@vip.qq.com>
2015-03-23 20:41:59 +0800

    add category-adding action

--- mvc/controller/admin/taxonomy.go
+++ mvc/controller/admin/taxonomy.go
@@ -26,3 +26,20 @@ func (tc *TaxonomyController) Get() {
 	}
 	tc.Render("taxonomy.html")
 }
+
+func (tc *TaxonomyController) Post() {
+	if tc.Req().FormValue("category") == "true" {
+		params := make(map[string]string)
+		params["name"] = tc.Req().FormValue("name")
+		params["slug"] = tc.Req().FormValue("slug")
+		params["desc"] = tc.Req().FormValue("desc")
+		params["uid"] = strconv.FormatInt(tc.AuthUser.Id, 10)
+		result := action.Call(action.CreateCategory, params)
+		if result.Meta.Status {
+			tc.Redirect("/admin/taxonomy?category=" + strconv.FormatInt(result.Data["category"].(*model.Category).Id, 10))
+			return
+		}
+		tc.Redirect("/admin/taxonomy?category=0")
+		return
+	}
+}
